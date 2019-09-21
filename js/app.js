03c0c8dbfa63bcd7636da248964f451e0a1bb12e
Kenold Beauplan <something4ken@gmail.com>
2019-09-21 18:01:46 -0400

    integrate handlebars JS library

---
+++ js/app.js
@@ -0,0 +1,274 @@
+var data = {
+    "posts": [
+        {
+            "id": 1,
+            "title": "All of Those Products Are Making Your Skin Worse",
+            "date": "7/16/2019",
+            "image": 1,
+            "category": "Jane Doe",
+            "excerpt": "Curabitur gravida nisi at nibh.",
+            "author": "Jane Doe",
+            "price": 82,
+            "sale_price": 43,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 2,
+            "title": "Young Women Are Leading The Global Climate Strike",
+            "date": "5/26/2019",
+            "image": 2,
+            "category": "Jean E. Gustave",
+            "excerpt": "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+            "author": "Jean E. Gustave",
+            "price": 96,
+            "sale_price": 38,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 3,
+            "title": "Young People Are Going to Save Us All From Office Life",
+            "date": "5/27/2019",
+            "image": 3,
+            "category": "Kenold Beauplan",
+            "excerpt": "Aliquam sit amet diam in magna bibendum imperdiet.",
+            "author": "Kenold Beauplan",
+            "price": 82,
+            "sale_price": 34,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 4,
+            "title": "The Top Milan Street Style Trends, From Leather To Mixed Prints",
+            "date": "1/24/2019",
+            "image": 4,
+            "category": "John Smith",
+            "excerpt": "Etiam vel augue.",
+            "author": "John Smith",
+            "price": 83,
+            "sale_price": 30,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 5,
+            "title": "Strike Cobra 3d Legging",
+            "date": "6/28/2019",
+            "image": 5,
+            "category": null,
+            "excerpt": "Nullam varius. Nulla facilisi.",
+            "author": null,
+            "price": 90,
+            "sale_price": 35,
+            "duration": null,
+            "artist": null,
+            "type": "Product"
+            }, {
+            "id": 6,
+            "title": "AE Denim Tie Front Crop Top",
+            "date": "8/30/2019",
+            "image": 6,
+            "category": null,
+            "excerpt": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+            "author": null,
+            "price": 93,
+            "sale_price": 37,
+            "duration": null,
+            "artist": null,
+            "type": "Product"
+            }, {
+            "id": 7,
+            "title": "Product 3",
+            "date": "4/1/2019",
+            "image": 7,
+            "category": null,
+            "excerpt": "Nulla ut erat id mauris vulputate elementum.",
+            "author": null,
+            "price": 82,
+            "sale_price": 48,
+            "duration": null,
+            "artist": null,
+            "type": "Product"
+            }, {
+            "id": 8,
+            "title": "Product 4",
+            "date": "3/24/2019",
+            "image": 8,
+            "category": null,
+            "excerpt": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+            "author": null,
+            "price": 88,
+            "sale_price": 48,
+            "duration": null,
+            "artist": null,
+            "type": "Product"
+            }, {
+            "id": 9,
+            "title": "Titanic",
+            "date": "2/6/2019",
+            "image": 9,
+            "category": null,
+            "excerpt": "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+            "author": null,
+            "price": 83,
+            "sale_price": 38,
+            "duration": null,
+            "artist": null,
+            "type": "Movie"
+            }, {
+            "id": 10,
+            "title": "Rambo",
+            "date": "3/14/2019",
+            "image": 10,
+            "category": null,
+            "excerpt": "Donec semper sapien a libero.",
+            "author": null,
+            "price": 89,
+            "sale_price": 33,
+            "duration": null,
+            "artist": null,
+            "type": "Movie"
+            }, {
+            "id": 11,
+            "title": "Dumb and Dummer",
+            "date": "7/28/2019",
+            "image": 11,
+            "category": null,
+            "excerpt": "Maecenas pulvinar lobortis est.",
+            "author": null,
+            "price": 90,
+            "sale_price": 42,
+            "duration": null,
+            "artist": null,
+            "type": "Movie"
+            }, {
+            "id": 12,
+            "title": "Shaft",
+            "date": "1/8/2019",
+            "image": 12,
+            "category": null,
+            "excerpt": "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
+            "author": null,
+            "price": 91,
+            "sale_price": 41,
+            "duration": null,
+            "artist": null,
+            "type": "Movie"
+            }, {
+            "id": 13,
+            "title": "Criminal Mind",
+            "date": "7/11/2019",
+            "image": 13,
+            "category": null,
+            "excerpt": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.",
+            "author": null,
+            "price": 100,
+            "sale_price": 47,
+            "duration": null,
+            "artist": null,
+            "type": "Movie"
+            }, {
+            "id": 14,
+            "title": "Friday Night",
+            "date": "12/5/2018",
+            "image": 14,
+            "category": null,
+            "excerpt": "Nunc rhoncus dui vel sem. Sed sagittis.",
+            "author": null,
+            "price": 83,
+            "sale_price": 40,
+            "duration": "4:23",
+            "artist": "Katty Perry",
+            "type": "Video"
+            }, {
+            "id": 15,
+            "title": "Umbrella",
+            "date": "10/14/2018",
+            "image": 15,
+            "category": null,
+            "excerpt": "Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+            "author": null,
+            "price": 90,
+            "sale_price": 41,
+            "duration": "3:23",
+            "artist": "Rihanna",
+            "type": "Video"
+            }, {
+            "id": 16,
+            "title": "You Need to Calm Down",
+            "date": "6/21/2019",
+            "image": 16,
+            "category": null,
+            "excerpt": "Mauris sit amet eros.",
+            "author": null,
+            "price": 88,
+            "sale_price": 36,
+            "duration": "4:32",
+            "artist": "Taylor Swift",
+            "type": "Video"
+            }, {
+            "id": 17,
+            "title": "Put a Ring On It",
+            "date": "9/3/2019",
+            "image": 17,
+            "category": null,
+            "excerpt": "Integer ac neque.",
+            "author": null,
+            "price": 80,
+            "sale_price": 43,
+            "duration": "5:15",
+            "artist": "Beyonce",
+            "type": "Video"
+            }, {
+            "id": 18,
+            "title": "All of Those Products Are Making Your Skin Worse",
+            "date": "1/8/2019",
+            "image": 18,
+            "category": "Jane Doe",
+            "excerpt": "Proin eu mi.",
+            "author": "Jane Doe",
+            "price": 82,
+            "sale_price": 36,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 19,
+            "title": "Young Women Are Leading The Global Climate Strike",
+            "date": "8/7/2019",
+            "image": 19,
+            "category": "Jean E. Gustave",
+            "excerpt": "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+            "author": "Jean E. Gustave",
+            "price": 96,
+            "sale_price": 49,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }, {
+            "id": 20,
+            "title": "Young People Are Going to Save Us All From Office Life",
+            "date": "4/29/2019",
+            "image": 20,
+            "category": "Kenold Beauplan",
+            "excerpt": "Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+            "author": "Kenold Beauplan",
+            "price": 92,
+            "sale_price": 44,
+            "duration": null,
+            "artist": null,
+            "type": "Blog"
+            }]
+}
+
+$(document).ready(function() {
+    // register handlebarsIntl helper
+    HandlebarsIntl.registerWith(Handlebars);
+
+    var dataTemplate = $("#data-template").html();
+    var compiledDataTemplate = Handlebars.compile(dataTemplate);
+    $(".data-list-container").html(compiledDataTemplate(data));
+
+});
