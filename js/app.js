486bc6384f93b76304cb37d2d7c5789d6a2a0fb7
Kenold Beauplan <bkenold@gmail.com>
2019-09-24 16:15:38 -0400

    move json data down

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
+$(document).ready(function() {
+
+  // register handlebarsIntl helper
+  HandlebarsIntl.registerWith(Handlebars);
+
+  var dataTemplate = $("#data-template").html();
+  var compiledDataTemplate = Handlebars.compile(dataTemplate);
+  $(".data-list-container").html(compiledDataTemplate(data));
+
+  // slick carousel
+  $('.carousel-3').slick({
+      mobileFirst: true,
+      slidesToShow: 1.5,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: false,
+      responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3.5,
+      slidesToScroll: 1,
+      arrows: true,
+      // dots: true,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2.5,
+      slidesToScroll: 1
+    }
+  }
+]
+  });
+
+});
+
+
+
 var data = {
     "posts": [
         {
@@ -276,42 +318,3 @@ var data = {
         }
     ]
 }
-
-$(document).ready(function() {
-    // register handlebarsIntl helper
-    HandlebarsIntl.registerWith(Handlebars);
-
-    var dataTemplate = $("#data-template").html();
-    var compiledDataTemplate = Handlebars.compile(dataTemplate);
-    $(".data-list-container").html(compiledDataTemplate(data));
-
-    // slick carousel
-    $('.carousel-3').slick({
-        mobileFirst: true,
-        slidesToShow: 1.5,
-        slidesToScroll: 1,
-        arrows: false,
-        infinite: false,
-        responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3.5,
-        slidesToScroll: 1,
-        arrows: true,
-        // dots: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2.5,
-        slidesToScroll: 1
-      }
-    }
-  ]
-    });
-
-});
