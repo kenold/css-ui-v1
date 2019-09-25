88b60ad7c68b52751c8dd516a9b42d350580791c
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 12:41:47 -0400

    import and display components

--- src/components/pages/cards.js
+++ src/components/pages/cards.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Blog from '../../components/cards/blog'
+import Product from '../../components/cards/product'
+import Movie from '../../components/cards/movie'
+import Video from '../../components/cards/video'
+import BlogWide from '../../components/cards/blogWide'
 
 class Cards extends Component {
     render() {
         return (
             <div className="container">
                 <Blog />
+                <Movie />
+                <Product />
+                <Video />
+                <BlogWide />
             </div>
         );
     }
