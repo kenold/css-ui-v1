ab909f3a78111bc76089c96e0b204b536aa79dd6
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 11:32:50 -0400

    initial commit

---
+++ src/components/pages/cards.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Blog from '../../components/cards/blog'
+
+class Cards extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Blog />
+            </div>
+        );
+    }
+}
+
+export default Cards;
