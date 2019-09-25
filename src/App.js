fbec46a445066c0c4df9b6356a04b4974c0a7093
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 10:42:14 -0400

    update text

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Blog from './components/cards/blog';
 function App() {
   return (
     <div className="App">
-    	<div className="container">
-			CSS UIs
-    	</div>
-		<Blog />
+    	<div class="container">
+			<section class="intro">
+				<h1>CSS UI Components</h1>
+				<h3>More info here</h3>
+			</section>
+			<ul>
+				<li><a href="card.html">Cards UI</a></li>
+			</ul>
+		</div>
     </div>
   );
 }
