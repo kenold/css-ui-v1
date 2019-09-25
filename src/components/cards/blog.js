353e4d43c4a2d4f17388587aa0c8c638ba9b94fd
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 11:32:42 -0400

    add card content

--- src/components/cards/blog.js
+++ src/components/cards/blog.js
@@ -3,7 +3,32 @@ import React from 'react';
 function Blog() {
   return (
     <div className="container">
-        Blog component
+        <section className="l-cards">
+			<div className="l-cards__heading">
+				<h4>Cards + Blog Posts </h4>
+			</div>
+			<div className="cards cards--blog">
+				<article className="card grid-4">
+					<a href="post.html">
+						<figure className="card__media">
+							<img src="images/thumbs/1.jpg" alt="title" className="card__image" />
+						</figure>
+						<div className="card__body">
+							<div className="card__category">
+								category
+							</div>
+							<div className="card__title" title="title">
+								<span>title</span>
+							</div>
+						</div>
+						<div className="card__footer">
+							<div className="card__author">author</div>
+							<div className="card__date">5 hours ago</div>
+						</div>
+					</a>
+				</article>
+			</div>
+		</section>
     </div>
   );
 }
