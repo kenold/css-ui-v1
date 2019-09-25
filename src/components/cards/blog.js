dd565896c58199632838dba525d0ebf9bed58ba9
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 12:41:33 -0400

    add card content

--- src/components/cards/blog.js
+++ src/components/cards/blog.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PostData from '../../data/posts.json';
 
-function Blog() {
-  return (
-    <div className="container">
-        <section className="l-cards">
-			<div className="l-cards__heading">
-				<h4>Cards + Blog Posts </h4>
+class Blog extends Component {
+	render() {
+		return (
+			<div className="container">
+				<section className="l-cards">
+					<div className="l-cards__heading">
+						<h4>Cards + Blog Posts </h4>
+					</div>
+					<div className="cards cards--blog">
+						{PostData.map((post) => (
+							<article key={post.id} className="card grid-4">
+								<a href="post.html">
+									<figure className="card__media">
+										<img src={"images/thumbs/" + post.id + ".jpg"} alt={post.title} className="card__image" />
+									</figure>
+									<div className="card__body">
+										<div className="card__category">{post.category}</div>
+										<div className="card__title" title={post.title}>
+											<span>{post.title}</span>
+										</div>
+									</div>
+									<div className="card__footer">
+										<div className="card__author">{post.author}</div>
+										<div className="card__date">5 hours ago</div>
+									</div>
+								</a>
+							</article>
+						))}
+					</div>
+				</section>
 			</div>
-			<div className="cards cards--blog">
-				<article className="card grid-4">
-					<a href="post.html">
-						<figure className="card__media">
-							<img src="images/thumbs/1.jpg" alt="title" className="card__image" />
-						</figure>
-						<div className="card__body">
-							<div className="card__category">
-								category
-							</div>
-							<div className="card__title" title="title">
-								<span>title</span>
-							</div>
-						</div>
-						<div className="card__footer">
-							<div className="card__author">author</div>
-							<div className="card__date">5 hours ago</div>
-						</div>
-					</a>
-				</article>
-			</div>
-		</section>
-    </div>
-  );
+		);
+	}
+
 }
 
 export default Blog;
