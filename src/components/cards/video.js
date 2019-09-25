405c65bd43e5960de6e9e2c481feee07beaba743
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 15:36:26 -0400

    comment out slick settings

--- src/components/cards/video.js
+++ src/components/cards/video.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import VideoData from '../../data/videos.json';
+import $ from "jquery";
 
 class Video extends Component {
 	render() {
+		// slick carousel
+		// $('.carousel-3').slick({
+		// 	mobileFirst: true,
+		// 	slidesToShow: 1.5,
+		// 	slidesToScroll: 1,
+		// 	arrows: false,
+		// 	infinite: false,
+		// 	responsive: [
+		// 	{
+		// 		breakpoint: 1024,
+		// 		settings: {
+		// 			slidesToShow: 3.5,
+		// 			slidesToScroll: 1,
+		// 			arrows: true,
+		// 		}
+		// 		},
+		// 		{
+		// 		breakpoint: 600,
+		// 		settings: {
+		// 			slidesToShow: 2.5,
+		// 			slidesToScroll: 1
+		// 		}
+		// 		}
+		// 	]
+		// });
 		return (
 			<div className="container">
 				<section className="l-cards">
 					<div className="l-cards__heading">
 						<h4>Cards + Video </h4>
 					</div>
-					<div className="cards">
+					<div className="cards carousel-3">
 						{VideoData.map((video) => (
 							<article key={video.id} className="card card--video">
 								<a href="video.html">
