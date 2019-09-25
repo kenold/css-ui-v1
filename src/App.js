d69fd6158f394cc389606f6871c8521925dbac8c
Kenold Beauplan <bkenold@gmail.com>
2019-09-25 11:32:28 -0400

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
-import Blog from './components/cards/blog';
+import Cards from './components/pages/cards';
 
-function App() {
-  return (
-    <div className="App">
-    	<div class="container">
-			<section class="intro">
-				<h1>CSS UI Components</h1>
-				<h3>More info here</h3>
-			</section>
-			<ul>
-				<li><a href="card.html">Cards UI</a></li>
-			</ul>
-		</div>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<div className="container">
+						<section className="intro">
+							<h1>CSS UI Components</h1>
+							<h3>More info here</h3>
+						</section>
+						<ul>
+							<li><Link to="/cards">Cards UI</Link></li>
+						</ul>
+					</div>
+
+					<Route exact path="/cards" component={Cards} />
+				</div>
+			</Router>
+		);
+	}
+
 }
 
 export default App;
