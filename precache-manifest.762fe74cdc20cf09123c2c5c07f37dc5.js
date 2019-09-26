59e084b1ff8be965120deb95b341c9c857423754
Kenold Beauplan <bkenold@gmail.com>
2019-09-26 12:28:55 -0400

    Updates

---
+++ precache-manifest.762fe74cdc20cf09123c2c5c07f37dc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de0c1037ba43dbaceb878a6e231ab1f",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "f0fd6b4c82288e5c01ac",
+    "url": "/ui/static/css/2.b39c55f2.chunk.css"
+  },
+  {
+    "revision": "18f828773a7e58be7718",
+    "url": "/ui/static/css/main.e9f54099.chunk.css"
+  },
+  {
+    "revision": "f0fd6b4c82288e5c01ac",
+    "url": "/ui/static/js/2.c8c575eb.chunk.js"
+  },
+  {
+    "revision": "18f828773a7e58be7718",
+    "url": "/ui/static/js/main.0e9a0ece.chunk.js"
+  },
+  {
+    "revision": "b570d949e37f6e074ecc",
+    "url": "/ui/static/js/runtime-main.cd03d53e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ui/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ui/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ui/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ui/static/media/slick.f97e3bbf.svg"
+  }
+]);
