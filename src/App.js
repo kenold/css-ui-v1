fa5bab32c006b695dd6645b7c889fe84639e243d
Kenold Beauplan <bkenold@gmail.com>
2019-09-26 12:02:39 -0400

    Revert "Merge branch 'master' of https://github.com/kenold/ui"
    This reverts commit 9b60bae44f02b2fc21390b221e9bc43f45114bf0, reversing
    changes made to 0b9a13d30a3a6ab1c71eb4586c230280392ee313.

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+} from 'react-router-dom';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart, faPlay } from '@fortawesome/free-solid-svg-icons';
+
+import Home from './components/home';
+import Cards from './components/pages/cards';
+
+library.add(faHeart, faPlay);
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<div className="container">
+						<section className="intro">
+							<h1>CSS UI Components</h1>
+							<h3>More info here</h3>
+						</section>
+						<ul>
+							<li><Link to="/">Home</Link></li>
+							<li><Link to="/cards">Cards UI</Link></li>
+						</ul>
+					</div>
+
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/cards" component={Cards} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+
+}
+
+export default App;
